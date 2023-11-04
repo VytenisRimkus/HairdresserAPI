21ae2ca095528cb0a9f3706e610683214dedd46b
Vytenis Rimkus <vytenis@atlantisgames.lt>
2023-11-04 16:10:05 +0200

    added migration

--- Migrations/20231104140946_AddCreatedbyUserId.Designer.cs
+++ Migrations/20231104140946_AddCreatedbyUserId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using HairdresserAPI.DatabaseContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HairdresserAPI.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231104140946_AddCreatedbyUserId")]
+    partial class AddCreatedbyUserId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20231104140946_AddCreatedbyUserId.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HairdresserAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCreatedbyUserId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CreatedByUserId",
+                table: "Hairdressers",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+
+            migrationBuilder.AddColumn<ulong>(
+                name: "IsCompleted",
+                table: "Booking",
+                type: "bit",
+                nullable: false,
+                defaultValue: 0ul);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedByUserId",
+                table: "Hairdressers");
+
+            migrationBuilder.DropColumn(
+                name: "IsCompleted",
+                table: "Booking");
+        }
+    }
+}
